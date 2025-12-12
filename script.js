91dd6e6ff6ffe485a75ddbf239af6b4d5ee172bd
sci223329-create <sci_223329@omu.edu.ly>
2025-12-12 18:39:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // ============================
+    // 1) التعامل مع الفورم (إرسال الرسالة)
+    // ============================
+    const form = document.querySelector("#contact form");
+    form.addEventListener("submit", function(e) {
+        e.preventDefault(); 
+        alert("تم إرسال رسالتك بنجاح! سنقوم بالتواصل معك قريباً.");
+    });
+
+    // ============================
+    // 2) عند الضغط على الصور (شراء عقار)
+    // ============================
+    const images = document.querySelectorAll("#listings img, #gallery img");
+
+    images.forEach(function(img) {
+        img.addEventListener("click", function() {
+
+            const propertyName = img.alt || "العقار";
+
+            const confirmPurchase = confirm(`هل تريد شراء ${propertyName}؟`);
+
+            if(confirmPurchase) {
+                alert(`تم تسجيل رغبتك في شراء ${propertyName}. سنتواصل معك قريباً!`);
+            }
+        });
+    });
+
+
+    // ============================
+    // 3) عند الضغط على العروض (Confirmation)
+    // ============================
+    const offers = document.querySelectorAll("#offers li");
+
+    const messages = [
+        "فيلا — خصم 10%",
+        "شقة عائلية — سعر خاص 300 د.ل / شهر",
+        "محل تجاري — فرصة استثمارية مع استشارة مجانية"
+    ];
+
+    offers.forEach((offers, index) => {
+        offers.addEventListener("click", () => {
+
+            let confirmOffer = confirm(`هل تريد اختيار العرض التالي؟\n\n${messages[index]}`);
+
+            if (confirmOffer) {
+                alert(`تم تسجيل رغبتك في عرض: ${messages[index]}. سنقوم بالتواصل معك قريباً!`);
+            }
+        });
+    });
+
+});
